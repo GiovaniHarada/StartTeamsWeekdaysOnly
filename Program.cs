66a482edfca761b6e26832382f195160db554906
Giovani Harada <giovaniharada@outlook.com>
2024-02-12 18:47:40 -0300

    working ;)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var today = (int)DateTime.Now.DayOfWeek;
+        if(today >= 1 && today <= 5) {
+            Console.WriteLine("Day of week, stating work apps...");
+            Process proc = new Process();
+            proc.StartInfo.FileName = "explorer.exe";
+            proc.StartInfo.Arguments = "shell:appsFolder\\MSTeams_8wekyb3d8bbwe!MSTeams";   
+            proc.Start(); 
+        } else {
+            Console.WriteLine("Weekend, no need for stating apps...");
+        }
+
+    }
+}
